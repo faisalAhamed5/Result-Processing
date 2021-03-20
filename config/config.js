4d0e368327583a408c2f43714bebcbab36283ea7
Faisal Ahamed Rahad <57454612+faisalAhamed5@users.noreply.github.com>
2021-03-20 14:10:29 +0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -12,13 +12,13 @@ module.exports = {
     "password": 'd29a5da44d8dca96ca7860a605bce06ac09464142de311d79c91aba7df91f064',
     "database": "dat4u3471vmmnl",
     "host": "ec2-52-6-178-202.compute-1.amazonaws.com",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
     "username": "itrrsbnqkoflxg",
     "password": 'd29a5da44d8dca96ca7860a605bce06ac09464142de311d79c91aba7df91f064',
     "database": "dat4u3471vmmnl",
     "host": "ec2-52-6-178-202.compute-1.amazonaws.com",
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 };
