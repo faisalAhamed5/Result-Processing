dea087703101fe63492bf353f6110c9503068516
Faisal Ahamed Rahad <57454612+faisalAhamed5@users.noreply.github.com>
2021-03-20 15:59:20 +0600

    g6

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: true
-  }
-});
+
 
 
 var createError = require('http-errors');
