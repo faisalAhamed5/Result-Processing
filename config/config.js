dca7d3a5826c6d0dd68d5bff2ab20e1102446008
Faisal Ahamed Rahad <57454612+faisalAhamed5@users.noreply.github.com>
2021-03-20 13:46:09 +0600

    Create config.js

--- config/config.js
+++ config/config.js
@@ -1,24 +1,24 @@
 module.exports = {
   "development": {
-    "username": "test",
-    "password": '123',
-    "database": "results",
-    "host": "127.0.0.1",
+    "username": "itrrsbnqkoflxg",
+    "password": 'd29a5da44d8dca96ca7860a605bce06ac09464142de311d79c91aba7df91f064',
+    "database": "dat4u3471vmmnl",
+    "host": "ec2-52-6-178-202.compute-1.amazonaws.com",
     "dialect": "postgres",
     "port": 5432
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "itrrsbnqkoflxg",
+    "password": 'd29a5da44d8dca96ca7860a605bce06ac09464142de311d79c91aba7df91f064',
+    "database": "dat4u3471vmmnl",
+    "host": "ec2-52-6-178-202.compute-1.amazonaws.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "itrrsbnqkoflxg",
+    "password": 'd29a5da44d8dca96ca7860a605bce06ac09464142de311d79c91aba7df91f064',
+    "database": "dat4u3471vmmnl",
+    "host": "ec2-52-6-178-202.compute-1.amazonaws.com",
     "dialect": "mysql"
   }
 };
